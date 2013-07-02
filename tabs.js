354cc7426c8413983ff5834eb80e14bdd5030f89
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-02 10:35:07 -0500

    adds basic models for Cookie and Oven

--- tabs.js
+++ tabs.js
@@ -1,5 +1,72 @@
-$(document).ready(function(){
+////////// MODELS  ///////
 
+var Oven = { 
+   contents : this.contents = [] ,
 
+   addFood : function(food){
+      contents.push(food) ;
+   },
+   bake : function(){
+      for (var i = 0; i < this.contents.length; i++) {
+         contents[i].actualTime += 1;
+      }
+   }
+};
+
+function Cookie(){
+   this.flavor = "oatmeal" ;
+   this.neededTime = 10 ;
+   this.actualTime = 0 ;
+};
+
+Cookie.prototype = {
+
+   cookieType : function(flavor){
+      this.flavor = flavor ;
+   },
+
+   bakeTime : function(time){
+      this.neededTime = time ;
+   }
 
 };
+
+Cookie.prototype.status = function(){
+   if (this.actualTime === 0){
+      return "raw"
+   } else if (this.actualTime < 5) {
+      return "gooey"
+   } else if (this.actualTime < 10) {
+      return "Good"
+   } else { 
+      return "Burnt"
+   };
+};
+
+/////// COOKIE TEST CODE
+var sugar = new Cookie ;
+sugar.cookieType("sugar");
+console.log(sugar.flavor);
+console.log(sugar.status());
+// sugar.actualTime = 10;
+// console.log(sugar.status())
+
+///////OVEN TEST CODE
+Oven.addFood(sugar);
+Oven.bake();
+Oven.bake();
+Oven.bake();
+console.log(sugar.status());
+console.log(Oven.contents);
+
+
+///// CONTROLLER ///////////////
+
+
+
+// $(document).ready(function(){
+
+
+
+
+// };
